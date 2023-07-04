ec87e5f69097115974b340c146f6845a4d843e31
M <m@mbp-m1-manu.home>
2023-07-04 13:06:08 +0200

    upgrade detail map

--- JO-PARIS-2024/Views/SportDetailView.swift
+++ JO-PARIS-2024/Views/SportDetailView.swift
@@ -15,62 +15,64 @@ struct SportDetailView: View {
     @ObservedObject var viewModel = ReadData()
     //
     @Binding var isPresented: Bool
-
+    
     var body: some View {
         ZStack {
-                    VStack {
-                        HStack {
-                            Button(action: {
-                                self.isPresented = false
-                            }) {
+            VStack {
+                VStack {
+                    HStack {
+                        Button(action: {
+                            self.isPresented = false
+                        }) {
+                            HStack {
+                                Image(systemName: "chevron.left")
+                                    .padding(.leading, 16)
+                                Spacer ()
+                                
+                                // Title du sport
                                 HStack {
-                                    Image(systemName: "chevron.left")
-                                        .padding(.horizontal, 16)
-                                    Spacer ()
-                                    HStack {
-                                        Text(sport.sports!)
-                                            .font(.title2)
-    //                                        .font(Font.custom("Paris2024-Variable", size: 24))
-                                            .foregroundColor(Color(red: 1, green: 0, blue: 0.23))
-    //                                        .padding(.horizontal, 15)
-                                    }
-                                    Spacer ()
+                                    Text(sport.sports!)
+//                                        .font(.title2)
+                                     .font(Font.custom("Paris2024-Variable", size: 24))
+                                        .foregroundColor(Color(red: 1, green: 0, blue: 0.23))
+                                        .padding(.leading, -32)
                                 }
+                                Spacer ()
                             }
-                            .padding(.bottom, 32)
+                            .multilineTextAlignment(.center)
                         }
-                        
-                        VStack {
-                    Text("")
-                        .font(.largeTitle)
-                            VStack{
-                                Text("Du \(viewModel.formatFrenchDate(date: sport.formattedStartDate!)) au \(viewModel.formatFrenchDate(date: sport.formattedEndDate!))")
-                                
-                                    .foregroundColor(Color("Zeus"))
-                                    .fontWeight(.semibold)
-//                                    .padding(.bottom, 8)
-                                
-                                Text("\n\(sport.awards ?? "")")
-                                    .foregroundColor(Color("Zeus").opacity(0.7))
-                                    .padding(.bottom, 24)
-                            }
+                        .padding(.bottom, 32)
+                    }
+                    
+                        VStack{
+                            Text("Du \(viewModel.formatFrenchDate(date: sport.formattedStartDate!)) au \(viewModel.formatFrenchDate(date: sport.formattedEndDate!))")
+                                .foregroundColor(Color("Zeus"))
+                                .fontWeight(.semibold)
+                            Text("\n\(sport.awards ?? "")")
+                                .foregroundColor(Color("Zeus").opacity(0.7))
+                                .padding(.bottom, 24)
+                        }
+//                        .padding(.bottom, 24)
                 }
-            
+                
                 // Carte avec le sport sélectionné
                 Map(coordinateRegion: $viewModel.defaultRegion, showsUserLocation: true, annotationItems: viewModel.sportsDatas.filter { $0.sports! == sport.sports }) { place in
-                            MapAnnotation(coordinate: place.coordinate!) { // Utilisez la propriété 'coordinate' du modèle
-                                VStack {
+                    MapAnnotation(coordinate: place.coordinate!) { // Utilisez la propriété 'coordinate' du modèle
+                        VStack {
                             //                            Image(systemName: "mappin")
                             Text("📍")
                                 .font(.title)
+//                                .foregroundColor(Color("RougeAmour"))
                             Text("\(place.localisation!)")
-                                .font(.system(size: 16))
+//                                .font(Font.custom("Paris2024-Variable"
                                 .foregroundColor(Color("Zeus"))
+                                .font(.system(size: 14))
                                 .fontWeight(.medium)
+                                .padding(4)
                                 .background(Color.white.opacity(0.95))
-                                .padding(32)
+                                
                         }
-                                .padding()
+                        //                        .padding()
                     }
                 }
                 .edgesIgnoringSafeArea(.all)
@@ -83,30 +85,29 @@ struct SportDetailView: View {
                     }
                     
                     else if sports.count == 2 || sports.count == 3 {
-                            // Calculer les moyennes de latitude et de longitude
-                            let sumLatitude = sports.compactMap({ $0.latitude }).reduce(0, +)
-                            let sumLongitude = sports.compactMap({ $0.longitude }).reduce(0, +)
-                            let meanLatitude = sumLatitude / Double(sports.count)
-                            let meanLongitude = sumLongitude / Double(sports.count)
-                                                
-                            // Calculer les écarts entre les latitudes et les longitudes
-                            let maxLatitude = sports.compactMap({ $0.latitude }).max() ?? meanLatitude
-                            let minLatitude = sports.compactMap({ $0.latitude }).min() ?? meanLatitude
-                            let maxLongitude = sports.compactMap({ $0.longitude }).max() ?? meanLongitude
-                            let minLongitude = sports.compactMap({ $0.longitude }).min() ?? meanLongitude
+                        // Calculer les moyennes de latitude et de longitude
+                        let sumLatitude = sports.compactMap({ $0.latitude }).reduce(0, +)
+                        let sumLongitude = sports.compactMap({ $0.longitude }).reduce(0, +)
+                        let meanLatitude = sumLatitude / Double(sports.count)
+                        let meanLongitude = sumLongitude / Double(sports.count)
+                        
+                        // Calculer les écarts entre les latitudes et les longitudes
+                        let maxLatitude = sports.compactMap({ $0.latitude }).max() ?? meanLatitude
+                        let minLatitude = sports.compactMap({ $0.latitude }).min() ?? meanLatitude
+                        let maxLongitude = sports.compactMap({ $0.longitude }).max() ?? meanLongitude
+                        let minLongitude = sports.compactMap({ $0.longitude }).min() ?? meanLongitude
                         let latitudeDelta = (maxLatitude - minLatitude) + 0.4
-                            let longitudeDelta = (maxLongitude - minLongitude) + 0.4
-                                                
-                            // Définir le centre de la carte à ces moyennes
-                            viewModel.defaultRegion.center = CLLocationCoordinate2D(latitude: meanLatitude, longitude: meanLongitude)
-                                                
-                            // Ajuster le niveau de zoom en fonction des écarts de coordonnées
-                            viewModel.defaultRegion.span = MKCoordinateSpan(latitudeDelta: latitudeDelta, longitudeDelta: longitudeDelta)
-                        }
-                    
+                        let longitudeDelta = (maxLongitude - minLongitude) + 0.4
+                        
+                        // Définir le centre de la carte à ces moyennes
+                        viewModel.defaultRegion.center = CLLocationCoordinate2D(latitude: meanLatitude, longitude: meanLongitude)
+                        
+                        // Ajuster le niveau de zoom en fonction des écarts de coordonnées
+                        viewModel.defaultRegion.span = MKCoordinateSpan(latitudeDelta: latitudeDelta, longitudeDelta: longitudeDelta)
+                    }
                     
                     else {
-                        viewModel.defaultRegion.span = MKCoordinateSpan(latitudeDelta: 11.0, longitudeDelta: 11.0)
+                        viewModel.defaultRegion.span = MKCoordinateSpan(latitudeDelta: 12.50, longitudeDelta: 12.50)
                         viewModel.defaultRegion.center = CLLocationCoordinate2D(latitude: 46.603354, longitude: 1.888334)
                     }
                 }
@@ -119,9 +120,9 @@ struct SportDetailView: View {
 
 
 /*
-struct SportDetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        SportDetailView(sport: SportsDataModel("XXX"), isPresented: .constant(true))
-    }
-}
-*/
+ struct SportDetailView_Previews: PreviewProvider {
+ static var previews: some View {
+ SportDetailView(sport: SportsDataModel("XXX"), isPresented: .constant(true))
+ }
+ }
+ */
